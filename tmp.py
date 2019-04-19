d318a0be5af0aa1d334dee570f0d3c7b5a756081
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2019-04-19 12:36:15 +0530

    Removed Unused files

--- tmp.py
+++
@@ -1,3 +0,0 @@
-n=int(input())
-input()
-for i in input()
--- tree.py
+++
@@ -1,6 +0,0 @@
-from stat_parser import Parser
-parser = Parser()
-f =open('tree.csv','w')
-for i in open('5_Ankur.csv').readlines():
-    f.write(' '.join(str(parser.parse(i)).split()))
-    f.write('\n')
